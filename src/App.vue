abfd28f785fdcc5c47f368c5ed8af0f3dd2f9db1
ebedoyaalzate <ebedoyaalzate@hotmail.com>
2019-10-01 17:24:41 -0500

    add register form

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  div(id="app")
+    router-view
 </template>
 
 <style lang="scss">
+@import './scss/application.scss';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,102 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div(class="auth-container")
+    img(class="logo", src="../assets/wolox_logo.svg", alt="Wolox logo")
+    p(class="subtitle") BOOKS
+    form(class="container-form" @submit.prevent="onSubmit")
+      label(class="label-input") First name
+      input(type="text", class="input-primary" v-model="firstName")
+
+      label(class="label-input") Last name
+      input(type="text", class="input-primary" v-model="lastName")
+
+      label(class="label-input") Email
+      input(type="email", class="input-primary" v-model="email")
+
+      label(class="label-input") Password
+      input(type="password", class="input-primary" v-model="password")
+
+      button(type="submit", class="button-primary") Sign up
+    button(type="button" class="button-secondary") Login
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null
+    };
+  },
+  methods: {
+    onSubmit() {
+      let user = {
+        first_name: this.firstName,
+        last_name: this.lastName,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.password,
+        locale: "en"
+      };
+      console.log({ user });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import '../scss/colors.scss';
+@import '../scss/buttons.scss';
+@import '../scss/inputs.scss';
+
+.container-form {
+  border-bottom: 3px solid $grey-soft;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  .label-input {
+    display: flex;
+    justify-content: flex-start;
+    font-size: 15px;
+    margin: 10px;
   }
 }
-</script>
+
+.auth-container {
+  align-items: center;
+  background-color: $grey;
+  border-top: 3px solid $blue;
+  display: flex;
+  flex-direction: column;
+  margin: 150px auto;
+  max-width: 350px;
+  padding: 10px;
+  width: 100%;
+
+  .logo {
+    height: 40px;
+    margin: 20px 20px 10px;
+  }
+
+  .subtitle {
+    font-size: 12px;
+    letter-spacing: 8px;
+  }
+}
+
+.alert {
+  background-color: $red;
+  border-radius: 10px;
+  color: $white;
+  padding: 10px;
+  width: 100%;
+}
+</style>
