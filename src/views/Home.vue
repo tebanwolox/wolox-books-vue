b1d0329d9507623d2e2125e9a9b59c52a86be77a
ebedoyaalzate <ebedoyaalzate@hotmail.com>
2019-10-02 16:13:06 -0500

    add vuelidate in form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,36 +4,70 @@
     p(class="subtitle") BOOKS
     form(class="container-form" @submit.prevent="onSubmit")
       label(class="label-input") First name
-      input(type="text", class="input-primary" v-model="firstName")
+      input(type="text", class="input-primary", v-model="$v.form.firstName.$model")
+      div(v-if="$v.form.firstName.$error")
+        span(class="alert" class="alert" v-if="!$v.form.firstName.required") This field is required
 
       label(class="label-input") Last name
-      input(type="text", class="input-primary" v-model="lastName")
+      input(type="text", class="input-primary" v-model="$v.form.lastName.$model")
+      div(v-if="$v.form.lastName.$error")
+        span(class="alert" class="alert" v-if="!$v.form.lastName.required") This field is required
 
       label(class="label-input") Email
-      input(type="email", class="input-primary" v-model="email")
+      input(type="text", class="input-primary" v-model="$v.form.email.$model")
+      div(v-if="$v.form.email.$error")
+        span(class="alert" class="alert" v-if="!$v.form.email.required") This field is required
+        span(class="alert" class="alert" v-if="!$v.form.email.email") This field is an email
 
       label(class="label-input") Password
-      input(type="password", class="input-primary" v-model="password")
+      input(type="password", class="input-primary" v-model="$v.form.password.$model")
+      div(v-if="$v.form.password.$error")
+        span(class="alert" class="alert" v-if="!$v.form.password.required") This field is required
+        span(class="alert" class="alert" v-if="!$v.form.password.strongPassword") The password must have uppercase letters and numbers
 
-      button(type="submit", class="button-primary") Sign up
+      button(type="submit", class="button-primary", :disabled="$v.form.$invalid") Sign up
     button(type="button" class="button-secondary") Login
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
+import { required, minLength, maxLength, email } from 'vuelidate/lib/validators'
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  },
   data: function() {
     return {
-      firstName: null,
-      lastName: null,
-      email: null,
-      password: null
+      form:{
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+      }
     };
+  },
+  validations: {
+    form: {
+      firstName: {
+        required
+      },
+      lastName: {
+        required
+      },
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        strongPassword(password) {
+          return (
+            /[a-z]/.test(password) &&
+            /[0-9]/.test(password) &&
+            /[A-Z]/.test(password)
+          );
+        }
+      }
+    }
   },
   methods: {
     onSubmit() {
@@ -48,13 +82,13 @@ export default {
       console.log({ user });
     }
   }
-};
+}
 </script>
 
 <style lang="scss">
-@import '../scss/colors.scss';
-@import '../scss/buttons.scss';
-@import '../scss/inputs.scss';
+@import "../scss/colors.scss";
+@import "../scss/buttons.scss";
+@import "../scss/inputs.scss";
 
 .container-form {
   border-bottom: 3px solid $grey-soft;
@@ -93,10 +127,9 @@ export default {
 }
 
 .alert {
-  background-color: $red;
   border-radius: 10px;
-  color: $white;
-  padding: 10px;
+  color: $red;
+  padding: 5px;
   width: 100%;
 }
 </style>
